917fcb9fd7efa8477a93b5a68a34223a87af08c2
khushboo yadav <mc21064@zealeducation.com>
2024-01-21 16:00:53 +0530

    Description of the changes

---
+++ src/test/java/RecipeBookApp/RecipeBookApp/RecipeBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package RecipeBookApp.RecipeBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
